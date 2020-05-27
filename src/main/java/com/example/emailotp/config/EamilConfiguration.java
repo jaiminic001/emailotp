a11fef90d62cbbf2d7968ce935807a57ea70742b
Pankaj <psharmam15@gmail.com>
2020-05-27 16:39:04 +0530

    jar

--- src/main/java/com/example/emailotp/config/EamilConfiguration.java
+++ src/main/java/com/example/emailotp/config/EamilConfiguration.java
@@ -1,36 +1,30 @@
 package com.example.emailotp.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
 
-@Configuration
+//@Configuration
 public class EamilConfiguration {
 
-    @Autowired
-    private ProviderConfiguration providerConfiguration;
+ //   @Autowired
+   // private ProviderConfiguration providerConfiguration;
 
-    @Bean
-    public JavaMailSender mailSender()
-    {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost(providerConfiguration.getHost());
-        javaMailSender.setPort(providerConfiguration.getPort());
+   // @Bean
+    //public JavaMailSender mailSender()
+    //{
+      //  JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+       // javaMailSender.setHost(providerConfiguration.getHost());
+       // javaMailSender.setPort(providerConfiguration.getPort());
 
-        javaMailSender.setUsername(providerConfiguration.getUsername());
-        javaMailSender.setPassword(providerConfiguration.getPassword());
+       // javaMailSender.setUsername(providerConfiguration.getUsername());
+       // javaMailSender.setPassword(providerConfiguration.getPassword());
 
-        Properties properties = javaMailSender.getJavaMailProperties();
-        properties.put("mail.transport.protocol", "smtp");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.debug", providerConfiguration.getDebug().toString());
+       // Properties properties = javaMailSender.getJavaMailProperties();
+       // properties.put("mail.transport.protocol", "smtp");
+       // properties.put("mail.smtp.auth", "true");
+       // properties.put("mail.smtp.starttls.enable", "true");
+       // properties.put("mail.debug", providerConfiguration.getDebug().toString());
 
-        return javaMailSender;
-    }
+        //return javaMailSender;
+    //}
 
 }
--- src/main/java/com/example/emailotp/services/EmailService.java
+++ src/main/java/com/example/emailotp/services/EmailService.java
@@ -1,6 +1,6 @@
 package com.example.emailotp.services;
 
-import com.starter.springboot.rest.dto.EmailDTO;
+import com.example.emailotp.rest.dto.EmailDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
